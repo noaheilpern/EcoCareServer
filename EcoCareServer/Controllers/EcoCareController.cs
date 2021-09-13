3de192d991921ebdd41300a88d16b00980f59bf1
User <User@LAB3-2>
2021-09-13 14:36:07 +0300

    Add project files.

---
+++ EcoCareServer/Controllers/EcoCareController.cs
@@ -0,0 +1,23 @@
+﻿using EcoCareServerBL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcoCareServer.Controllers
+{
+    [Route("EcoCareAPI")]
+    [ApiController]
+    public class EcoCareController : ControllerBase
+    {
+        #region Add connection to the db context using dependency injection
+        EcoCareDBContext context;
+        public EcoCareController(EcoCareDBContext context)
+        {
+            this.context = context;
+        }
+        #endregion
+    }
+}
